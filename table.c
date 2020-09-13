d734e4aee719bac8f99e2936c93ed5942be50f65
ramsaycarslaw <ramsaycarslaw@icloud.com>
2020-09-13 11:34:26 +0100

    Added interned strings at runtime
    Should speed it up

--- table.c
+++ table.c
@@ -140,3 +140,30 @@ void tableAddAll(Table* from, Table* to)
 		}
 	}
 }
+
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash)
+{
+	if (table->count == 0) return NULL;
+
+	uint32_t index = hash % table->capacity;
+
+	for (;;)
+	{
+		Entry* entry = &table->entries[index];
+		
+		if (entry->key == NULL)
+		{
+	    // Stop if we find an empty non-tombstone entry.
+	    if (IS_NIL(entry->value)) return NULL;
+		}
+		else if (entry->key->length == length &&
+			 entry->key->hash == hash &&
+			 memcmp(entry->key->chars, chars, length) == 0)
+		{
+			// We found it.
+			return entry->key;
+		}
+
+		index = (index + 1) % table->capacity;
+	}
+}
