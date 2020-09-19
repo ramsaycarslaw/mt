9d7ee8cdec27930014b25d10f2b66ca8097fe0e7
ramsaycarslaw <ramsaycarslaw@icloud.com>
2020-09-19 10:35:03 +0100

    remove ccls cache

--- .ccls-cache/@Users@ramsaycarslaw@dev@C@mt/compiler.h
+++
@@ -1,8 +0,0 @@
-#ifndef mt_compiler_h
-#define mt_compiler_h
-
-#include "vm.h"
-
-int compile(const char * src, Chunk *chunk);
-
-#endif
--- .ccls-cache/@Users@ramsaycarslaw@dev@C@mt/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef mt_debug_h
-#define mt_debug_h
-
-#include "chunk.h"
-
-void disassembleChunk(Chunk* chunk, const char* name);
-int disassembleInstruction(Chunk* chunk, int offset);
-
-#endif
