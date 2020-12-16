f1a7d03a4962e8615f72b6a70f2b2656a7f30003
ramsaycarslaw <ramsaycarslaw@icloud.com>
2020-12-16 15:48:51 +0000

    fix headers

---
+++ docs/.auctex-auto/docs.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "docs"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "report"
+    "rep10"
+    "microtype"
+    "listings"
+    "hyperref"
+    "lstautogobble"
+    "color"
+    "zi4")
+   (LaTeX-add-color-definecolors
+    "bluekeywords"
+    "greencomments"
+    "redstrings"
+    "graynumbers"))
+ :latex)
+
--- native.h
+++ native.h
@@ -1,28 +1,29 @@
 #ifndef mt_native_h
 #define mt_native_h
 
+#include <dirent.h>
 #include <time.h>
 
-#include "value.h"
 #include "object.h"
+#include "value.h"
 #include "vm.h"
 
-Value clockNative(int argCount, Value* args);
-Value readNative(int argCount, Value* args);
-Value writeNative(int argCount, Value* args);
-Value randIntNative(int argcount, Value* args);
-Value inputNative(int argCount, Value* args);
-Value doubleNative(int argCount, Value* args);
-Value stringNative(int argCount, Value* args);
-Value exitNative(int argCount, Value* args);
-Value clearNative(int argCount, Value* args);
-Value showNative(int argCount, Value* args);
+Value clockNative(int argCount, Value *args);
+Value readNative(int argCount, Value *args);
+Value writeNative(int argCount, Value *args);
+Value randIntNative(int argcount, Value *args);
+Value inputNative(int argCount, Value *args);
+Value doubleNative(int argCount, Value *args);
+Value stringNative(int argCount, Value *args);
+Value exitNative(int argCount, Value *args);
+Value clearNative(int argCount, Value *args);
+Value showNative(int argCount, Value *args);
 Value cdNative(int argCount, Value *args);
 Value lsNative(int argCount, Value *args);
-Value printfNative(int argCount, Value* args);
-Value printlnNative(int argCount, Value* args);
-Value colorSetNative(int argCount, Value* args);
-Value appendNative(int argCount, Value* args);
-Value deleteNative(int argCount, Value* args);
+Value printfNative(int argCount, Value *args);
+Value printlnNative(int argCount, Value *args);
+Value colorSetNative(int argCount, Value *args);
+Value appendNative(int argCount, Value *args);
+Value deleteNative(int argCount, Value *args);
 Value lenNative(int argCount, Value *args);
 #endif
